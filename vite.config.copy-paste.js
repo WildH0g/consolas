894722f11de1cc65b62d93ca94be07a35b8b09b4
WildH0g <dmitry.kostyuk@gmail.com>
2024-05-04 08:10:08 +0200

    Fix copy-paste compile

--- vite.config.copy-paste.js
+++ vite.config.copy-paste.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import { AppsScriptPlugin } from './vite-plugin/vite-plugin-appsscript.js';
 
 export default defineConfig({
+  plugins: [AppsScriptPlugin('dist/copy-paste/consolas.iife.js', 'ConsolAS')],
   build: {
     minify: true,
     outDir: resolve(process.cwd(), 'dist/copy-paste'),
